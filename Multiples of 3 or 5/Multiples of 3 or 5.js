6d294c927187d21d531a821342e4b138c379d244
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-18 23:40:31 +0300

    write solution

--- Multiples of 3 or 5/Multiples of 3 or 5.js
+++ Multiples of 3 or 5/Multiples of 3 or 5.js
@@ -4,6 +4,16 @@ Titile ========
 */
 
 // Answer
-
+function solution(number){
+  let sum = 0
+  for (let i = 0; i < number; i++) {
+    if(i % 3 === 0) {
+      sum += i
+    } else if(i % 5 === 0) {
+      sum += i
+    }
+  }
+  return sum
+}
 
 // Codewars: codewars.com/kata/514b92a657cdc65150000006/train/javascript
