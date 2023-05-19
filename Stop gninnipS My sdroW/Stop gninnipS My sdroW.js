36fe0cc40180308a8b874dea8e609d8e9bc991db
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-19 23:41:33 +0300

    write solution

--- Stop gninnipS My sdroW/Stop gninnipS My sdroW.js
+++ Stop gninnipS My sdroW/Stop gninnipS My sdroW.js
@@ -5,6 +5,12 @@ Title ===========
 */
 
 // Answer
-
+function spinWords(string){
+    let strArr = string.split(" ")
+    let res = []
+    let a = strArr.map((el) => el.length >= 5 ? res.push(el.split("").reverse().join("")): res.push(el))
+    return res.join(" ")
+  }
+              
 
 // Codewars: https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
