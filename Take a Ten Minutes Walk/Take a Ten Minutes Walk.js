9b9674acafc2d322f4e43363215b9dcaeb7ebc94
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-23 23:40:30 +0300

    write solution

--- Take a Ten Minutes Walk/Take a Ten Minutes Walk.js
+++ Take a Ten Minutes Walk/Take a Ten Minutes Walk.js
@@ -4,6 +4,16 @@ Title ========
 */
 
 // Answer
+function isValidWalk(walk) {
+  let ns = 0, we = 0; 
+    for (let dir of walk) { 
+      if (dir == 'n') ns += 1; 
+      if (dir == 's') ns -= 1; 
+      if (dir == 'w') we += 1; 
+      if (dir == 'e') we -= 1; 
+    } 
 
+    return walk.length == 10 && ns === 0 && we === 0; 
+}
 
 // Codewars: https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
